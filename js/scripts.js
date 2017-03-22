980f5ebed84527c71203cbfbe956b3916a992c50
mygosia <mygosia@gmail.com>
2017-03-22 19:32:01 +0100

    done

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var a = 7;
+    h = 10;
+    triangleArea = a*h/2 ;
+
+console.log("pole trójkąta o podstawie a = " + a + " i wysokości h = " + h + " wynosi:"  + triangleArea);
+
+alert(triangleArea);
