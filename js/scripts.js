e6a63ef30e4760b3c7f5c2318b4de4a3b2c423ad
mygosia <mygosia@gmail.com>
2017-03-22 20:42:41 +0100

    done

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var a=-7;
+    b = -8;
+    value = ((a*a) + (2*a*b) - (b-b));
+
+console.log(value);
+
+
+if (value < 0) {
+    console.log("wynik ujemny")
+  } 
+else if (value=0) {
+  console.log("ZERO")
+}
+else {
+    console.log("wynik dodatni")
+  }
+
+/*
+value > 0 ? console.log("wynik dodatni") : console.log("wynik ujemny")
+
+if (value=0) {
+  console.log("ZERO")
+}
+*/
