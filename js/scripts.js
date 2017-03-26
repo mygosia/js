2805a7c2b089cc9755891155bc7605ffd615af1a
mygosia <mygosia@gmail.com>
2017-03-26 11:56:49 +0200

    nie działa - why?

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var getButton = document.getElementsByClassName('button');
+console.log(getButton);
+
+
+var getButton = document.getElementsByClassName('button');
+var buttonText = getButton.innerText;
+console.log(buttonText);
+//for ( var x = 0  ; x < getButton.length    ; x = x + 1   ) {
+//	console.log(getButton.innerText);
+//};
