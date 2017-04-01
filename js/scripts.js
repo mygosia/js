9ff5249a514bc87ae9614c4afc4cc3c08c1cbf1e
mygosia <mygosia@gmail.com>
2017-04-01 20:05:24 +0200

    dziala

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,5 @@
-var getButton = document.getElementsByClassName('button');
-console.log(getButton);
+var getButton = document.getElementsByClassName('mybutton');
 
-
-var getButton = document.getElementsByClassName('button');
-var buttonText = getButton.innerText;
-console.log(buttonText);
-//for ( var x = 0  ; x < getButton.length    ; x = x + 1   ) {
-//	console.log(getButton.innerText);
-//};
+for ( var x = 0  ; x < getButton.length ; x = x + 1   ) {
+	console.log(getButton[x].innerText);
+};
